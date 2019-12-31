0785b0fecddfeb20e80a75cca7aa2ee728847bab
PRIYANSHUSHARMAPHULERA <58679005+PRIYANSHUSHARMAPHULERA@users.noreply.github.com>
2019-12-31 09:34:01 +0530

    Create conversion of tuple in list and vice versa.py

---
+++ conversion of tuple in list and vice versa.py
@@ -0,0 +1,11 @@
+tuple1=(101,102,103,104,105)
+L=list(tuple1)#change tuple in list
+print(L)
+L.append(106)
+print(L)
+L.insert(4,5)
+print(L)
+L.remove(101)
+print(L)
+tuple2=tuple(L)#list in tuple
+print(tuple2)
